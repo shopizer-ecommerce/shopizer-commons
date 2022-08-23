3ca27de71bb8e3de29a126bd96839e97e0f09004
shopizerecomm <csamson777@yahoo.com>
2022-08-23 11:37:55 -0400

    search keyword

--- src/main/java/modules/commons/search/request/SearchItem.java
+++ src/main/java/modules/commons/search/request/SearchItem.java
@@ -3,6 +3,13 @@ package modules.commons.search.request;
 public class SearchItem extends IndexItem {
 
 	private static final long serialVersionUID = 1L;
+	private String suggestions;
+	public String getSuggestions() {
+		return suggestions;
+	}
+	public void setSuggestions(String suggestions) {
+		this.suggestions = suggestions;
+	}
 	
 
 
